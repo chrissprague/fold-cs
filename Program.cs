27931c3b3f9226765884219e77a3a6ffb1a7a536
Chris Sprague <css7209@rit.edu>
2014-10-25 02:19:00 -0400

    progress on reading input
    trying to emulate fold's actual functionality / handling of args as
    possible

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace fold_cs
 
         public const uint DEFAULT_WIDTH = 80;
 
+        private static uint getWidthFromInput(string input)
+        {
+            uint width = 80;
+            if ( "".Equals(input) ) // also handles null
+            {
+                // TODO: Error message
+                return 0;
+            }
+
+            try
+            {
+                // TODO: catch uint overflow ( >~ 4x10E9 )
+                width = uint.Parse(input);
+            }
+            catch (FormatException ex)
+            {
+                Console.Error.WriteLine("ERROR: Invalid width value.");
+                Console.Error.WriteLine(ex.StackTrace);
+                // TODO: exit with non-zero error code here
+            }
+            return width;
+        }
+
         static void Main(string[] args)
         {
             /* TODO: Implement these flags
@@ -18,35 +41,52 @@ namespace fold_cs
              */
 
             uint width = DEFAULT_WIDTH;
+            bool measureInBytes = false;
+            bool retainWordStructure = false;
 
             // read command line arguments
             bool nextArgumentSpecifiesWidth = false;
+            bool readingFiles = false;
             foreach ( string argument in args )
             {
-                if ( nextArgumentSpecifiesWidth )
+                // User is specifying a flag argument to the program.
+                if ( ( argument[0] == '-' ) && ( argument.Length >= 2 ) )
                 {
-                    try
+                    for ( byte i = 1 ; i < argument.Length ; ++i )
                     {
-                        // TODO: catch uint overflow ( >~ 4x10E9 )
-                        width = uint.Parse(argument);
-                        Console.WriteLine(width);
-                    } catch (FormatException ex )
-                    {
-                        Console.Error.WriteLine("ERROR: Invalid width value.");
-                        Console.Error.WriteLine(ex.StackTrace);
+                        char flag = argument[i];
+                        if ( nextArgumentSpecifiesWidth )
+                        {
+                            // if width value not separated from the 'w' flag with a space
+                            // example: 'fold -w10 test.txt'
+                            width = getWidthFromInput(argument.Substring(i));
+                            nextArgumentSpecifiesWidth = false;
+                            break;
+                        }
+                        if ( ! ( flag == 'w' ) || ( flag == 's' ) || ( flag == 'b' ) )
+                        {
+                            Console.Error.WriteLine("ERROR: Invalid flag '{0}'.", flag);
+                        } else
+                        {
+                            if (flag == 'b')
+                                measureInBytes = true;
+                            if (flag == 's')
+                                retainWordStructure = true;
+                            if (flag == 'w')
+                                nextArgumentSpecifiesWidth = true;
+                        }
                     }
-                }
-                // specifying width - check next argument
-                if ( argument.Equals("-w") )
+                } else if ( nextArgumentSpecifiesWidth )
                 {
-                    nextArgumentSpecifiesWidth = true;
+                    nextArgumentSpecifiesWidth = false;
+                    width = getWidthFromInput(argument);
                 }
-                // Console.WriteLine(argument);
             }
+
+            Console.WriteLine("The width is: "+width);
 
             // pause program for debugging.
             string read = Console.ReadLine();
-            Console.WriteLine(read);
         }
     }
 }
